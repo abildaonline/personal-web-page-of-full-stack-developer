4d3a265fd71084b517fc87ee075b8c902524f15c
abildaonline <abildabexultan@gmail.com>
2024-05-04 13:02:58 +0500

    first commit

---
+++ slider.js
@@ -0,0 +1,11 @@
+$('.works-block').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true
+});
+
+$('.certificates').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true
+});
